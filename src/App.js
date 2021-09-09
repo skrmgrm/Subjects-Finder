8dbf0aa3f65b53af8f3c876d6c84947df3421058
Joshua Barlolong <joshuabarlolong@gmail.com>
2021-09-09 18:17:11 +0800

    refactored code, added gclassroom code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
@@ -6,13 +6,134 @@ import Admin from "./components/pages/Admin";
 
 import Main from "./components/pages/Main";
 
+import axios from "axios";
+import Subject from "./components/pages/Subject";
+
 function App() {
+  const [code, setCode] = useState("");
+  const [gClassroomCode, setGclassroomCode] = useState("");
+  const [section, setSection] = useState("");
+  const [description, setDescription] = useState("");
+  const [schedule, setSchedule] = useState("");
+  const [link, setLink] = useState("");
+
+  const [subjects, setSubjects] = useState([]);
+
+  const fetchSubjects = useCallback(async () => {
+    try {
+      const response = await axios.get(
+        "https://subjects-finder-default-rtdb.asia-southeast1.firebasedatabase.app/subjects.json"
+      );
+
+      let newSubjects = [];
+
+      for (const key in response.data) {
+        newSubjects.push({
+          id: key,
+          code: response.data[key].code,
+          section: response.data[key].section,
+          description: response.data[key].description,
+          schedule: response.data[key].schedule,
+          link: response.data[key].link,
+          gClassroomCode: response.data[key].gClassroomCode,
+        });
+      }
+
+      setSubjects(newSubjects.reverse());
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchSubjects();
+  }, [fetchSubjects]);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(
+        "https://subjects-finder-default-rtdb.asia-southeast1.firebasedatabase.app/subjects.json",
+        {
+          code,
+          gClassroomCode,
+          description,
+          schedule,
+          link,
+          section,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+
+    fetchSubjects();
+    setCode("");
+    setGclassroomCode("");
+    setSection("");
+    setDescription("");
+    setSchedule("");
+    setLink("");
+  };
+
+  const onDeleteHandler = (key) => {
+    axios
+      .delete(
+        `https://subjects-finder-default-rtdb.asia-southeast1.firebasedatabase.app/subjects/${key}.json`
+      )
+      .then(() => {
+        fetchSubjects();
+      });
+  };
+
+  const handleSubmitCode = (e) => {
+    setCode(e);
+  };
+
+  const handleSubmitSection = (e) => {
+    setSection(e);
+  };
+
+  const handleSubmitDescription = (e) => {
+    setDescription(e);
+  };
+
+  const handleSubmitSchedule = (e) => {
+    setSchedule(e);
+  };
+
+  const handleSubmitLink = (e) => {
+    setLink(e);
+  };
+
+  const handleSubmitGclassroom = (e) => {
+    setGclassroomCode(e);
+  };
+
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/admin" exact component={Admin} /> 
+          <Route path="/" exact>
+            <Main subjects={subjects} />
+          </Route>
+          <Route path="/admin" exact>
+            <Admin
+              subjects={subjects}
+              onSubmitHandler={onSubmitHandler}
+              handleSubmitCode={handleSubmitCode}
+              handleSubmitLink={handleSubmitLink}
+              handleSubmitDescription={handleSubmitDescription}
+              handleSubmitSection={handleSubmitSection}
+              handleSubmitSchedule={handleSubmitSchedule}
+              handleSubmitGclassroom={handleSubmitGclassroom}
+              onDelete={onDeleteHandler}
+            />
+          </Route>
+          <Route path="/subjects/:id" exact>
+            <Subject subjects={subjects} />
+          </Route>
         </Switch>
       </div>
     </Router>
--- src/components/Item.js
+++ src/components/Item.js
@@ -5,6 +5,8 @@ import SubjectTitle from "./UI/Card/SubjectTitle";
 import SubjectSchedule from "./UI/Card/SubjectSchedule";
 import SubjectSection from "./UI/Card/SubjectSection";
 
+import { Link } from "react-router-dom";
+
 const Item = (props) => {
   return (
     <Card>
@@ -18,14 +20,12 @@ const Item = (props) => {
             <SubjectSection>{props.section}</SubjectSection>
           </div>
           <div className="col-md-2">
-            <a
-              href={props.link}
-              target="_blank"
+            <Link
+              to={`/subjects/${props.id}`}
               className="btn btn-primary w-100 my-3 my-md-5 "
-              rel="noreferrer"
             >
-              Join
-            </a>
+              View Details
+            </Link>
           </div>
         </div>
       </div>
